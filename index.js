c4db8c882cb8a36e59b3b5c1dfe25d55c51564c8
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-04-15 23:41:30 +0530

    second commit

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-// console.log("sub");
-// var a=20;
-// var b=30;
-// var c=40;
-// console.log(a+b+c);
+// // console.log("sub");
+// // var a=20;
+// // var b=30;
+// // var c=40;
+// // console.log(a+b+c);
 
 
-// var x=20;
-// if(x==20)
-// {
-//     console.log("Matched")
-// }
+// // var x=20;
+// // if(x==20)
+// // {
+// //     console.log("Matched")
+// // }
 
-// var x='20';
-// for(i=0;i<=10;i++)
-// {
-//     console.log(i);
-// }
+// // var x='20';
+// // for(i=0;i<=10;i++)
+// // {
+// //     console.log(i);
+// // }
 
-const app = require('./app')
-const arr=[2,4,7,1,3,8,3];
-// console.log(arr[0]);
+// const app = require('./app')
+// const arr=[2,4,7,1,3,8,3];
+// // console.log(arr[0]);
 
-// import {x} from './app'
-console.log(app)
-console.log(app.x)
-console.log(app.y)
-console.log(app.z())
+// // import {x} from './app'
+// console.log(app)
+// console.log(app.x)
+// console.log(app.y)
+// console.log(app.z())
 
-// Filter function can be used to find a particular value from an array.
+// // Filter function can be used to find a particular value from an array.
 
-arr.filter((item) => {
-    console.log(item)
-})
+// arr.filter((item) => {
+//     console.log(item)
+// })
 
-let result = arr.filter((item) => {
-    return item>=4
-})
-console.warn(result);
+// let result = arr.filter((item) => {
+//     return item>=4
+// })
+// console.warn(result);
