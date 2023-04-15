432af6af35d8d3208df1b9286594b040a6c7beb2
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-04-15 23:54:14 +0530

    Learned core modules

--- index1.js
+++ index1.js
@@ -1,1 +1,9 @@
-console.log("Taru")
+// const fs= require('fs');
+// console.log("Taru");
+// fs.writeFileSync("hello.txt","taru")
+// console.log(">>",__dirname);//can write __filename
+
+// //OR
+
+const fs=require('fs').writeFileSync;
+fs("abc.txt",'abc') //we can write gs also instead of fs
