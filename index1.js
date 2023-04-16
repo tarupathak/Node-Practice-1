b2f145b0d713642da08525e6eddc2fbf0f0d5dea
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-04-17 00:25:01 +0530

    Learned servar handle request and response and learned how to make a basic server

--- index1.js
+++ index1.js
@@ -1,9 +1,8 @@
-// const fs= require('fs');
-// console.log("Taru");
-// fs.writeFileSync("hello.txt","taru")
-// console.log(">>",__dirname);//can write __filename
+ // const fs= require('fs');
+ // console.log("Taru"); 
+ // fs.writeFileSync("hello.txt","taru") // console.log(">>",__dirname);//can write __filename
 
-// //OR
+//OR
 
-const fs=require('fs').writeFileSync;
-fs("abc.txt",'abc') //we can write gs also instead of fs
+// const fs=require('fs').writeFileSync;
+// fs("abc.txt",'abc') //we can write gs also instead of fs
